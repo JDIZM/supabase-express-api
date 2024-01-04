347e820ef28e2f364da324c8d59851eef251866c
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-04 07:10:19 +0000

    feat: add created date with timestamp to users schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,8 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   fullName: text("full_name"),
-  phone: varchar("phone", { length: 256 })
+  phone: varchar("phone", { length: 256 }),
+  createdAt: timestamp("created_at", { precision: 6, withTimezone: true }).defaultNow()
 });
