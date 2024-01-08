8c2088b3fdc0fd3879e486987a5d9c1b2c442ad3
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-08 08:10:13 +0000

    feat: update schema and add zod validation

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,22 @@
 import { pgTable, uuid, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const users = pgTable("users", {
   uuid: uuid("uuid").defaultRandom().primaryKey(),
-  fullName: text("full_name"),
+  fullName: text("full_name").notNull(),
   phone: varchar("phone", { length: 256 }),
   createdAt: timestamp("created_at", { precision: 6, withTimezone: true }).defaultNow(),
   email: text("email").unique().notNull()
+  // TODO add role to schema and add permissions middleware
+  // role: text("role", { enum: ["admin", "user"] }).notNull()
 });
+
+// Schema for inserting a user - can be used to validate API requests
+export const insertUserSchema = createInsertSchema(users);
+
+// Schema for selecting a user - can be used to validate API responses
+export const selectUserSchema = createSelectSchema(users);
+
+// Validate UUID
+export const uuidSchema = z.object({ uuid: z.string().uuid() });
