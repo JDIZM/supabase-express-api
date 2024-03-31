a9d4de1c018c6af27592946f6ce13345d33bbe86
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-03-31 11:57:09 +0100

    feat: add logger class

---
+++ src/helpers/logger.test.ts
@@ -0,0 +1,34 @@
+import { logger } from "./logger";
+import { describe, it, expect, vi } from "vitest";
+
+describe("logger", () => {
+  it("should log a debug message", () => {
+    const spy = vi.spyOn(console, "debug");
+    logger.debug("debug message");
+    expect(spy).toBeCalledWith("debug message");
+  });
+
+  it("should log an info message", () => {
+    const spy = vi.spyOn(console, "info");
+    logger.info("info message");
+    expect(spy).toBeCalledWith("info message");
+  });
+
+  it("should log a warn message", () => {
+    const spy = vi.spyOn(console, "warn");
+    logger.warn("warn message");
+    expect(spy).toBeCalledWith("warn message");
+  });
+
+  it("should log an error message", () => {
+    const spy = vi.spyOn(console, "error");
+    logger.error("error message");
+    expect(spy).toBeCalledWith("error message");
+  });
+
+  it("should log a debug message with data", () => {
+    const spy = vi.spyOn(console, "debug");
+    logger.debug("debug message", { data: "data" });
+    expect(spy).toBeCalledWith("debug message", [{ data: "data" }]);
+  });
+});
