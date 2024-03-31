56662810302b8d3d9b6683963a1e89898cbed7c0
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-03-31 14:09:51 +0100

    test: update logger test

--- src/helpers/logger.test.ts
+++ src/helpers/logger.test.ts
@@ -5,30 +5,30 @@ describe("logger", () => {
   it("should log a debug message", () => {
     const spy = vi.spyOn(console, "debug");
     logger.debug("debug message");
-    expect(spy).toBeCalledWith("debug message");
+    expect(spy).toBeCalledWith("DEBUG: debug message");
   });
 
   it("should log an info message", () => {
     const spy = vi.spyOn(console, "info");
     logger.info("info message");
-    expect(spy).toBeCalledWith("info message");
+    expect(spy).toBeCalledWith("INFO: info message");
   });
 
   it("should log a warn message", () => {
     const spy = vi.spyOn(console, "warn");
     logger.warn("warn message");
-    expect(spy).toBeCalledWith("warn message");
+    expect(spy).toBeCalledWith("WARN: warn message");
   });
 
   it("should log an error message", () => {
     const spy = vi.spyOn(console, "error");
     logger.error("error message");
-    expect(spy).toBeCalledWith("error message");
+    expect(spy).toBeCalledWith("ERROR: error message");
   });
 
   it("should log a debug message with data", () => {
     const spy = vi.spyOn(console, "debug");
     logger.debug("debug message", { data: "data" });
-    expect(spy).toBeCalledWith("debug message", [{ data: "data" }]);
+    expect(spy).toBeCalledWith("DEBUG: debug message", [{ data: "data" }]);
   });
 });
