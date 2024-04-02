fd167cb2a6b287970f7ce38c5e0918cfb7ed003e
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-04-02 07:38:13 +0100

    feat: add sign up with supabase option when seeding users and associate id's with db user

--- src/services/db/seed.ts
+++ src/services/db/seed.ts
@@ -3,6 +3,26 @@ import { logger } from "../../helpers/logger.ts";
 
 async function main(): Promise<void> {
   logger.info("Seeding users...");
+
+  const args = process.argv.slice(2);
+
+
+  const options = args
+    .map((str) => str.replace(/^-+/, "").split("="))
+    .reduce<{ [key: string]: string }>((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
+
+  // Note if you are using supabase you will need to confirm the email addresses.
+  if (options?.supabase) {
+    logger.info("Seeding users with supabase...");
+    const ids = await seedUsers(true);
+    logger.info("Created users with ids:", ids);
+    return;
+  }
+
   const ids = await seedUsers();
   logger.info("Created users with ids:", ids);
 }
--- src/services/db/seeds/users.ts
+++ src/services/db/seeds/users.ts
@@ -1,16 +1,19 @@
 import { db } from "@/services/db/drizzle.js";
 import { users } from "@/schema.js";
-
+import { signUpWithSupabase } from "@/handlers/auth/auth.handlers.ts";
 import { InferInsertModel } from "drizzle-orm";
+import { logger } from "@/helpers/logger.ts";
 
 const usersArray: InferInsertModel<typeof users>[] = [
   {
+    uuid: "00000000-0000-0000-0000-000000000000",
     fullName: "John Doe",
     phone: "555-555-5555",
     email: "john.doe@example.com",
     role: "admin"
   },
   {
+    uuid: "00000000-0000-0000-0000-000000000001",
     fullName: "Jane Doe",
     phone: "555-555-5555",
     email: "jane.doe@example.com",
@@ -20,11 +23,22 @@ const usersArray: InferInsertModel<typeof users>[] = [
 
 async function createUser(user: InferInsertModel<typeof users>) {
   const response = await db.insert(users).values(user).returning();
-  console.log("created user: ", response);
+  logger.debug("created user: ", response);
   // we know that the response will be an array of one element
   return response[0].uuid;
 }
 
-export function seedUsers() {
+export async function seedUsers(isSupabase = false) {
+  if (isSupabase) {
+    const users = await Promise.all(usersArray.map((user) => signUpWithSupabase(user.email, "example-password")));
+    logger.debug("signed up users and created uuids", users);
+
+    users.forEach((user, index) => {
+      if (user) {
+        usersArray[index].uuid = user.id;
+      }
+    });
+  }
+
   return Promise.all(usersArray.map((user) => createUser(user)));
 }
