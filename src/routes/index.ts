683c7c4aa14985871467a8dafbb5bc9455e72d84
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-06-15 16:17:59 +0100

    feat: update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import type { Application } from "express";
-import { test } from "@/helpers/index.js";
+import { test, permissions } from "@/helpers/index.ts";
+import {
+  fetchWorkspacesHandler,
+  createWorkspaceHandler,
+  fetchWorkspaceHandler
+} from "@/handlers/workspaces/workspaces.handlers.ts";
 import { signInWithPassword, signUp } from "@/handlers/auth/auth.handlers.js";
-import { getUser, getUsers, createUser } from "@/handlers/users/users.handlers.js";
-import { API_ROUTES } from "@/helpers/permissions.js";
-import { isAuthenticated } from "@/middleware/isAuthenticated.js";
-import { isAuthorized } from "@/middleware/isAuthorized.js";
+import { getAccount, getAccounts, createAccount, updateAccount } from "@/handlers/accounts/accounts.handlers.ts";
+import { isAuthenticated } from "@/middleware/isAuthenticated.ts";
+import { isAuthorized } from "@/middleware/isAuthorized.ts";
+import { createProfile, getProfiles } from "@/handlers/profiles/profiles.handlers.ts";
+
+const { API_ROUTES } = permissions;
 
 export function routes(app: Application) {
   // @ts-expect-error no-unused-parameters
@@ -13,7 +20,17 @@ export function routes(app: Application) {
   });
   app.post(API_ROUTES.login, isAuthenticated, isAuthorized, signInWithPassword);
   app.post(API_ROUTES.signUp, isAuthenticated, isAuthorized, signUp);
-  app.get(API_ROUTES.users, isAuthenticated, isAuthorized, getUsers);
-  app.get(API_ROUTES.userById, isAuthenticated, isAuthorized, getUser);
-  app.post(API_ROUTES.usersCreate, isAuthenticated, isAuthorized, createUser);
+  app.get(API_ROUTES.accounts, isAuthenticated, isAuthorized, getAccounts);
+  app.post(API_ROUTES.accounts, isAuthenticated, isAuthorized, createAccount);
+  app.get(API_ROUTES.accountById, isAuthenticated, isAuthorized, getAccount);
+  app.put(API_ROUTES.accountById, isAuthenticated, isAuthorized, updateAccount);
+  app.patch(API_ROUTES.accountById, isAuthenticated, isAuthorized, updateAccount);
+
+  app.get(API_ROUTES.profiles, isAuthenticated, isAuthorized, getProfiles);
+  app.post(API_ROUTES.profileById, isAuthenticated, isAuthorized, createProfile);
+  app.get(API_ROUTES.profilesAll, isAuthenticated, isAuthorized, getProfiles);
+
+  app.get(API_ROUTES.workspacesAll, isAuthenticated, isAuthorized, fetchWorkspacesHandler);
+  app.post(API_ROUTES.workspaces, isAuthenticated, isAuthorized, createWorkspaceHandler);
+  app.get(API_ROUTES.workspaceById, isAuthenticated, isAuthorized, fetchWorkspaceHandler);
 }
