69282a14f497735047002596a96446d8418f5330
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-06-15 23:02:28 +0100

    feat: type guard user role when creating membership

--- src/handlers/memberships/memberships.handlers.ts
+++ src/handlers/memberships/memberships.handlers.ts
@@ -1,12 +1,21 @@
 import type { Request, Response } from "express";
+import type { Role } from "@/helpers/permissions/permissions.ts";
 import { workspaceMemberships } from "@/schema.ts";
 import { db } from "@/services/db/drizzle.ts";
-import type { Role } from "@/helpers/permissions/permissions.ts";
+import { logger } from "@/helpers/index.ts";
+import { and, eq } from "drizzle-orm";
+
+// Type guard function
+export function isValidRole(role: string): role is "admin" | "user" {
+  return ["admin", "user"].includes(role);
+}
 
 export async function createMembership(workspaceId: string, accountId: string, role: Role) {
-  if (role !== "admin" || role !== "user") {
+  if (!isValidRole(role)) {
     throw new Error("Invalid role");
   }
+
+  logger.info({ msg: `Creating membership for ${accountId} in ${workspaceId} as ${role}` });
 
   const membership = await db
     .insert(workspaceMemberships)
@@ -17,7 +26,7 @@ export async function createMembership(workspaceId: string, accountId: string, r
     })
     .returning();
 
-  console.log("created membership: ", membership);
+  logger.info({ msg: `Created membership for ${accountId} in ${workspaceId} as ${role}` });
 
   return membership[0];
 }
@@ -26,7 +35,7 @@ export async function createMembership(workspaceId: string, accountId: string, r
  * Check if the user is a member of the workspace.
  */
 export async function checkMembership(accountId: string, workspaceId: string): Promise<[boolean, string]> {
-  console.log("checkMembership", accountId, workspaceId);
+  logger.info({ msg: `Checking membership for ${accountId} in ${workspaceId}` });
 
   if (!accountId || !workspaceId) {
     return [false, ""];
@@ -40,12 +49,18 @@ export async function checkMembership(accountId: string, workspaceId: string): P
 
   const isMember = (result?.accountId === accountId && result?.workspaceId === workspaceId) || false;
 
+  logger.info({
+    msg: `Checked membership for ${accountId} in ${workspaceId}. User is [${isMember}, ${result?.role ?? ""}]`
+  });
+
   return [isMember, result?.role ?? ""];
 }
 
 export async function createMembershipHandler(req: Request, res: Response) {
   try {
     const { workspaceId, accountId, role } = req.body;
+
+    logger.info({ msg: `Creating membership for ${accountId} in ${workspaceId} as ${role}` });
 
     const membership = await createMembership(workspaceId, accountId, role);
 
