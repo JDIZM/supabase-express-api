215894f811d5675bedafba60f652d13655a5f3bc
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-06-18 00:51:53 +0100

    refactor auth middleware

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -1,13 +1,12 @@
-import { permissions } from "@/helpers/permissions.ts";
-import { supabase } from "@/services/supabase.ts";
-import { db } from "@/services/db/drizzle.js";
-import { logger } from "@/helpers/logger.ts";
-import { users } from "@/schema.js";
+import { logger, gatewayResponse, permissions } from "@/helpers/index.ts";
+// import { supabase } from "@/services/supabase.ts";
+import { db } from "@/services/db/drizzle.ts";
+import { accounts } from "@/schema.ts";
 import { eq } from "drizzle-orm";
-import type { Route } from "@/helpers/permissions.ts";
+import type { Route } from "@/helpers/index.ts";
 import type { NextFunction, Request, Response } from "express";
+import type { Method } from "@/helpers/permissions/permissions.ts";
 
-// https://stackabuse.com/bytes/how-to-get-a-users-ip-address-in-express-js/
 const getIpFromRequest = (req: Request): string | undefined => {
   const ips =
     req.headers["cf-connecting-ip"] ?? req.headers["x-real-ip"] ?? req.headers["x-forwarded-for"] ?? req.ip ?? "";
@@ -22,58 +21,66 @@ export const isAuthenticated = async (req: Request, res: Response, next: NextFun
   const token = authHeader && authHeader.split(" ")[1];
 
   const routeKey = (req.baseUrl + req.route.path) as Route;
-  const routePermissions = permissions.get(routeKey);
+  const routeMethod = req.method as Method;
+
+  const resourcePermissions = permissions.permissions.get(routeKey);
+  const requiresAuth = (resourcePermissions && resourcePermissions.authenticated) || false;
 
   const ips = getIpFromRequest(req);
-  logger.info("ip address:", ips);
 
-  // parsed cookies from cookie-parser middleware
-  logger.info("cookies", req.cookies);
+  logger.debug({ msg: "isAuthenticated middleware", ips, routeKey, routeMethod, resourcePermissions, requiresAuth });
 
-  if (!routePermissions?.length) {
+  if (!requiresAuth) {
+    logger.debug({ msg: "isAuthenticated: No authentication required", routeKey });
+
     return next();
   }
 
-  if (!token) {
-    logger.error("A token is required for authentication", 403);
+  if (!token && requiresAuth) {
+    logger.error({ msg: "isAuthenticated: A token is required for authentication", routeKey, routeMethod });
+
     return res.status(403).send("A token is required for authentication");
   }
 
   try {
     // Verify token using your auth service.
-    const {
-      data: { user },
-      error
-    } = await supabase.auth.getUser(token);
+    // TODO use local db user.
+    // const user = { id: "5f29e851-e7a1-4cf2-8e5a-5037a85a4224" }; // admin and super admin
+    const user = { id: "e2abbf9a-093b-426a-a078-09256517934d" }; // user
 
-    if (error || !user) {
-      throw new Error("Supabase User not found", {
-        cause: error
-      });
+    // TODO - uncomment this when using supabase.
+    // const {
+    //   data: { user },
+    //   error
+    // } = await supabase.auth.getUser(token);
+    //
+
+    // if (error || !user) {
+    //   throw new Error("User not found", {
+    //     cause: error,
+    //   });
+    // }
+
+    logger.debug({ msg: `Verified user token for id: ${user.id}` });
+
+    // get account from DB.
+    const [account] = await db.select().from(accounts).where(eq(accounts.uuid, user.id)).execute();
+
+    if (!account) {
+      throw new Error("DB User not found");
     }
 
-    // get user from DB.
-    const [dbUser] = await db.select().from(users).where(eq(users.uuid, user.id)).execute();
-
-    if (!dbUser) {
-      throw new Error("DB User not found", {
-        cause: error
-      });
-    }
+    logger.debug({ msg: `Fetched account from db with id: ${account.uuid}` });
 
     // Attach user to res.locals and verify permissions in isAuthorized middleware
-    res.locals = { id: user.id, sub: user.id, claims: dbUser.role };
+    res.locals = { id: user.id, sub: user.id, account };
 
-    // TODO Remove this example res.locals
-    // Example res.locals with user info to test isAuthorized middleware
-    // res.locals = {
-    //   id: "f30ebcf1-ae80-4e09-b04b-39ca23856825",
-    //   sub: "f30ebcf1-ae80-4e09-b04b-39ca23856825",
-    //   claims: ["admin"]
-    // };
     return next();
   } catch (error) {
-    logger.error("isAuthenticated error:", 403, error);
-    return res.status(403).send(error);
+    const response = gatewayResponse().error(403, error as Error, "isAuthenticated error");
+
+    logger.error({ msg: "isAuthenticated error:", response });
+
+    return res.status(response.code).send(response);
   }
 };
--- src/middleware/isAuthorized.ts
+++ src/middleware/isAuthorized.ts
@@ -1,46 +1,121 @@
-import { permissions } from "@/helpers/permissions.ts";
-import { logger } from "@/helpers/logger.ts";
 import type { NextFunction, Request, Response } from "express";
-import type { Route } from "@/helpers/permissions.ts";
+import type { Route } from "@/helpers/index.ts";
+import type { Method } from "@/helpers/permissions/permissions.ts";
+import { logger, permissions } from "@/helpers/index.ts";
+import { checkMembership } from "@/handlers/memberships/memberships.handlers.ts";
+import { getProfileById } from "@/handlers/profiles/profiles.methods.ts";
+
+const ResourceType = {
+  ACCOUNT: "account",
+  PROFILE: "profile"
+} as const;
+
+export function determineResourceType(route: Route) {
+  const keys = Object.values(ResourceType);
+  const resourceType = keys.find((key) => route.includes(key));
+  return resourceType ?? "";
+}
+
+/**
+ * Check if the user is the owner of a resource.
+ */
+const isOwner = async (id: string, resourceId: string, resourceType: string): Promise<boolean> => {
+  switch (resourceType) {
+    case ResourceType.ACCOUNT:
+      return id === resourceId;
+
+    case ResourceType.PROFILE: {
+      const [profile] = await getProfileById(resourceId);
+
+      if (profile) {
+        logger.debug({ msg: "isOwner: profile", id, resourceId, accountId: profile.accountId });
+
+        return profile.accountId === id;
+      }
+
+      return false;
+    }
+
+    default:
+      return false;
+  }
+};
 
 export const isAuthorized = async (req: Request, res: Response, next: NextFunction) => {
-  const { id, sub, claims } = res.locals;
-  console.debug("res.locals", res.locals);
+  const { id, sub, account } = res.locals;
 
+  const routeMethod = req.method as Method;
   const routeKey = (req.baseUrl + req.route.path) as Route;
-  const routePermissions = permissions.get(routeKey);
-  const isOwner: boolean = (id && (req.params?.userId === id || req.params?.id === id)) || false;
+  const workspaceId = req.headers["x-workspace-id"];
 
-  logger.debug("routeKey", routeKey);
-  logger.debug("routePermissions", routePermissions);
-  logger.debug("isOwner", isOwner);
+  logger.debug({ msg: `Authorizing for workspace id: ${workspaceId}` });
+  logger.debug({ msg: "isAuthorized: res.locals", locals: res.locals });
 
-  // If the route doesn't have any permissions, allow access.
-  if (!routePermissions?.length) {
-    return next();
-  }
+  const resourcePermissions = permissions.permissions.get(routeKey);
+  const resourcePermission = resourcePermissions && resourcePermissions.permissions[routeMethod];
 
-  // If the user is not logged in, deny access.
-  if (!sub || !claims || !id || !claims.length) {
+  const isSuperAdmin: boolean = account?.isSuperAdmin || false;
+
+  logger.debug({
+    msg: "isAuthorized: middleware",
+    routeKey,
+    routeMethod,
+    workspaceId,
+    resourcePermissions,
+    resourcePermission,
+    isSuperAdmin
+  });
+
+  if (!sub || !id || !account) {
+    logger.error({ msg: "Unauthorized user", id, routeKey, resourcePermission, routeMethod, workspaceId });
+
     return res.status(401).send("Unauthorized");
   }
 
-  // If the user is an admin, allow access.
-  if (claims.includes("admin")) {
+  if (!routeMethod) {
+    logger.error({ msg: "isAuthorized: Route method not allowed", routeKey, workspaceId });
+
+    return res.status(405).send("Route method not allowed");
+  }
+
+  // Super admin only has access to routes that require super admin permissions.
+  if (resourcePermissions?.super && isSuperAdmin) {
+    logger.debug({
+      msg: `isAuthorized: Super admin for account id: ${account.uuid}`,
+      routeKey,
+      workspaceId,
+      isSuperAdmin
+    });
+
+    return next();
+  } else if (resourcePermissions?.super) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  if (!resourcePermission) {
+    logger.debug({ msg: "isAuthorized: No permissions required", routeKey, workspaceId, isSuperAdmin });
+
     return next();
   }
 
-  // User permissions
-  if (claims.includes("user")) {
-    if (routePermissions.includes("user")) {
-      return next();
-    }
+  // Ensure the user is a member of the workspace and has the required role by validating the x-workspace-id header.
+  const [isMember, role] = await checkMembership(id, workspaceId as string);
 
-    if (routePermissions.includes("owner") && isOwner) {
-      return next();
-    }
+  logger.debug({ msg: "isAuthorized: checkMembership", isMember, role });
 
-    logger.error("Unauthorized user", 401, id, routeKey, routePermissions);
-    return res.status(401).send("Unauthorized");
+  if (isMember && (resourcePermission.includes(role) || resourcePermission.includes("user"))) {
+    return next();
   }
+
+  // Check if the user is the owner of the resource
+  const resourceType = determineResourceType(routeKey);
+
+  const isUserOwner = await isOwner(id, req.params?.id || "", resourceType);
+
+  if (isUserOwner && resourcePermission.includes("owner")) {
+    logger.debug({ msg: "isAuthorized: Owner" });
+    return next();
+  }
+
+  return res.status(403).json({ message: "Forbidden" });
 };
