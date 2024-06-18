f3660eb8b677b6f7f0bc982e1a31e23af0730a70
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-06-18 22:22:03 +0100

    chore: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,25 +4,32 @@ module.exports = {
     es2022: true,
     node: true
   },
+  parser: "@typescript-eslint/parser",
   extends: [
     // By extending from a plugin config, we can get recommended rules without having to add them manually.
     "eslint:recommended",
     "plugin:import/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
     "eslint-config-prettier",
     "prettier"
   ],
+  plugins: ["@typescript-eslint", "import"],
   settings: {
     // Tells eslint how to resolve imports
     "import/resolver": {
       // using the newer eslint-import-resolver-typescript plugin
       // see: https://www.npmjs.com/package/eslint-import-resolver-typescript
-      node: true,
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
+      },
       typescript: {
-        alwaysTryTypes: true
-        // project: "./tsconfig.json" // relative to project root.
+        alwaysTryTypes: true,
+        directory: "./tsconfig.json"
       }
     },
     "import/parsers": {
@@ -32,7 +39,7 @@ module.exports = {
   rules: {
     // Add your own rules here to override ones from the extended configs.
     "@typescript-eslint/no-explicit-any": "warn",
-    "arrow-parens": "error"
+    "arrow-parens": ["error", "always"]
   },
   overrides: [
     {
