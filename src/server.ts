092f549d361327c6b65a47cb53ecbec8cddeb4ed
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2025-04-28 14:47:45 +0100

    feat: add health endpoint

--- src/server.ts
+++ src/server.ts
@@ -51,6 +51,15 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Pre-flight requests
 
+app.get("/health", (_req: Request, res: Response) => {
+  const data = {
+    uptime: process.uptime(),
+    message: "Ok",
+    date: new Date()
+  };
+  res.status(200).send(data);
+});
+
 // Define routes
 routes(app);
 
